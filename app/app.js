2a321d543de19058e118c6a89aa2cb83f89c8bfb
yotam shalev <yotamsha@gmail.com>
2017-05-17 18:42:14 +0300

    Fixed CORS issue with the API request.

--- app/app.js
+++ app/app.js
@@ -6,8 +6,15 @@ angular.module('myApp', [
     'ngMaterial',
     'myApp.guess-the-artist',
     'myApp.services'
-]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+]).config(['$locationProvider', '$routeProvider','$sceDelegateProvider', function ($locationProvider, $routeProvider, $sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+        // Allow loading from our assets domain.  Notice the difference between * and **.
+        'https://itunes.apple.com/**'
+    ]);
     $locationProvider.hashPrefix('!');
+
 
     $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- app/common/services/itunes-api.service.js
+++ app/common/services/itunes-api.service.js
@@ -2,14 +2,16 @@
  * Created by yotam on 22/12/2016.
  */
 angular.module('myApp.services')
-    .service('ItunesApiService', ['$http', function ($http) {
+    .service('ItunesApiService', ['$http','$sce', function ($http,$sce) {
         var API_PREFIX = 'https://itunes.apple.com/';
         //var API_PREFIX = https://itunes.apple.com/search?term=Jack Johnson&entity=album
 
         var Service = {
             getAlbumsByArtist: function (artist, limit) {
-                return $http.get(API_PREFIX + '/search?entity=album' +
-                    '&term=' + artist)
+                var url = API_PREFIX + 'search?entity=album' +
+                    '&term=' + artist;
+                $sce.trustAsResourceUrl(url);
+                return $http.jsonp(url,  {jsonpCallbackParam: 'callback'})
                     .then(function (result) {
                         return result.data;
                     });
